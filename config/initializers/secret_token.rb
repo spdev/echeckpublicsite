89994a2591bc25acfa72eb068a46f3646b73d9c2
Austin007 <austin@sweetperk.com>
2014-03-09 23:07:47 -0500

    first drop of website

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Echeckwebsite::Application.config.secret_token = 'bc836c29bc4c25a205ab4813f35ef02770d8c9e28e88073aab6f866c6688d221250df6a3b2f727ea86212cdbf804e29639394bb93e513e08df717f5cdbdfe461'
